e47d3313894d8c80d92243f3edaedc539889d882
unknown <lennartpaduch99@gmail.com>
2025-06-14 18:10:52 +0200

    Added solution to: Hops

---
+++ Level 2/Hops/sol.js
@@ -0,0 +1,30 @@
+
+/**
+ * @param {number} N
+ * @param {number} F
+ * @param {number[]} P
+ * @return {number}
+ */
+function getSecondsRequired(N, F, P) {
+    let gaps = 0;
+
+    P.sort(((a, b) => b - a))
+
+    const gapToLast = N - P[0] -
+        1
+
+
+    for (let i = 0; i < P.length - 1; i++) {
+        const diff = P[i] - P[i + 1];
+        gaps += diff - 1
+    }
+
+
+
+    return gaps + P.length + gapToLast
+}
+
+N = 3
+F = 1
+P = [1]
+console.log(getSecondsRequired(N, F, P))
